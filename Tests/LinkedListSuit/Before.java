0cdcfa44000585617c82880c6f80f0dd3d1a4f56
Moritz <ninj24o@gmail.com>
2021-05-08 14:04:24 +0200

    Created Annotations and fixed Syntax Errors

---
+++ Tests/LinkedListSuit/Before.java
@@ -0,0 +1,5 @@
+package LinkedListSuit;
+
+public @interface Before {
+
+}
--- Tests/LinkedListSuit/ListTester.java
+++ Tests/LinkedListSuit/ListTester.java
@@ -1,8 +1,6 @@
-package de.dsa.Tests.LinkedList;
+package LinkedListSuit;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import de.dsa.datastructures;
+import LinkedList.LinkedList;
 
 public class ListTester {
 
@@ -14,19 +12,19 @@ public class ListTester {
 	public static void main(String[] args) {
 
 		// Testsuit for append
-		List l = List.empty();
+		LinkedList l = LinkedList.empty();
 		l = testAppend(l, 4);
 
 		// Testsuit for copy
-		List f = List.copy(l);
+		LinkedList f = LinkedList.copy(l);
 		System.out.println("ID for f: " + f + ", ID for l: " + l);
 		print(f);
-		f = List.append(f, 42);
+		f = LinkedList.append(f, 42);
 		print(f);
 		print(l);
 
 		// Testsuit for concat
-		List c = new List(0, null);
+		LinkedList c = new LinkedList(0, null);
 		c = setupConcat(c);
 		l = testConcat(l, c);
 	}
@@ -36,17 +34,17 @@ public class ListTester {
 	 * 
 	 * @param a the list to be printed
 	 */
-	public static void print(List a) {
+	public static void print(LinkedList a) {
 
 		System.out.print("(");
-		if (!List.isEmpty(a)) {
-			System.out.print(List.first(a));
-			a = List.rest(a);
+		if (!LinkedList.isEmpty(a)) {
+			System.out.print(LinkedList.first(a));
+			a = LinkedList.rest(a);
 		}
-		while (!List.isEmpty(a)) {
+		while (!LinkedList.isEmpty(a)) {
 
-			System.out.print(", " + List.first(a));
-			a = List.rest(a);
+			System.out.print(", " + LinkedList.first(a));
+			a = LinkedList.rest(a);
 		}
 		System.out.println(")");
 	}
@@ -59,9 +57,9 @@ public class ListTester {
 	 * @return The mutated startList
 	 */
 	@Test
-	public static List testAppend(List startList, int times) {
+	public static LinkedList testAppend(LinkedList startList, int times) {
 		for (int i = 1; i < times + 1; i++) {
-			startList = List.append(startList, i);
+			startList = LinkedList.append(startList, i);
 
 			System.out.format("Testing Append with: %d: ", i);
 			print(startList);
@@ -75,10 +73,10 @@ public class ListTester {
 	 * @param c empty list to be generated for concat
 	 * @return filled list
 	 */
-	@BeforeEach
-	public static List setupConcat(List c) {
+	@Before
+	public static LinkedList setupConcat(LinkedList c) {
 		for (int i = 1; i < 3; i++) {
-			c = new List(i, c);
+			c = new LinkedList(i, c);
 		}
 
 		System.out.print("Done with setting up c: ");
@@ -94,8 +92,8 @@ public class ListTester {
 	 * @return list l that was concatted
 	 */
 	@Test
-	public static List testConcat(List l, List c) {
-		l = List.concat(l, c);
+	public static LinkedList testConcat(LinkedList l, LinkedList c) {
+		l = LinkedList.concat(l, c);
 
 		System.out.print("Test concat: ");
 		print(l);
