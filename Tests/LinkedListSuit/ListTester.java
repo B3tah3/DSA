9aee074d19785f773dfa9a71be72f8b85a8bb347
Moritz <ninj24o@gmail.com>
2021-05-08 15:41:57 +0200

    Added Tests for insertAt, removeIndex, moveFromTo

--- Tests/LinkedListSuit/ListTester.java
+++ Tests/LinkedListSuit/ListTester.java
@@ -12,21 +12,156 @@ public class ListTester {
 	public static void main(String[] args) {
 
 		// Testsuit for append
+		System.out.println("TESTING APPEND");
 		LinkedList l = LinkedList.empty();
 		l = testAppend(l, 4);
+		printSeperator();
 
 		// Testsuit for copy
+		System.out.println("TESTING COPY");
+		
 		LinkedList f = LinkedList.copy(l);
 		System.out.println("ID for f: " + f + ", ID for l: " + l);
-		print(f);
 		f = LinkedList.append(f, 42);
+		
+		System.out.print("Changed f: ");
 		print(f);
+		System.out.print("Copied l: ");
 		print(l);
+		
+		printSeperator();
 
 		// Testsuit for concat
+		System.out.println("TESTING CONCAT");
 		LinkedList c = new LinkedList(0, null);
 		c = setupConcat(c);
 		l = testConcat(l, c);
+		printSeperator();
+		
+		//Testsuit for insertAt
+		System.out.println("TESTING INSERTAT");
+		testInsertAt();
+		printSeperator();
+		
+		//Test for removeIndex
+		System.out.println("TESTING REMOVEINDEX");
+		testRemoveIndex();
+		printSeperator();
+		
+		//Testsuit for moveFromTo
+		System.out.println("TESTING MOVEFROMTO");
+		testMoveFromTo();
+		printSeperator();
+		
+	}
+	
+	@Test
+	private static void testMoveFromTo() {
+		LinkedList m;
+		m = initLinkedList();
+		
+		print(m);
+		System.out.println();
+		
+		for(int n = 0; n<6; n++) {
+			try {
+			
+			m = initLinkedList();
+			System.out.print(String.format("Moving index %d to front: ", n));
+			m = LinkedList.moveFromTo(n, 0, m);
+			print(m);
+			
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println(e);
+			}
+		}
+		
+		System.out.println();
+		m = initLinkedList();
+		
+		for(int i = 0; i<6; i++) {
+			for(int j = 0; j<6; j++) {
+				try {
+					m = initLinkedList();
+					m = LinkedList.moveFromTo(i, j, m);
+					System.out.format("Moving Index %d to Index %d: ", i, j);
+					print(m);
+				} catch(IndexOutOfBoundsException e) {
+					System.out.format("Moving Index %d to Index %d: ", i, j);
+					System.out.println(e);
+				}
+			}
+		}
+	}
+	
+	@Test
+	private static void testRemoveIndex() {
+		LinkedList m;
+		m = initLinkedList();
+		
+		for(int n = 0; n<6; n++) {
+			try {
+			
+			m = initLinkedList();
+			System.out.print(String.format("Removing index %d: ", n));
+			m = LinkedList.removeIndex(n, m);
+			print(m);
+			
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println(e);
+			}
+		}
+		
+		m = new LinkedList(1, null);
+		m = LinkedList.removeIndex(0, m);
+		System.out.print("Removing element from list length 1: ");
+		print(m);
+	}
+	
+	@Test
+	private static void testInsertAt() {
+		LinkedList m = initLinkedList();
+		LinkedList insert = new LinkedList(100, null);
+		
+		System.out.print("Original List: ");
+		print(m);
+		
+		for(int i = 0; i < 7; i++) {
+			try {
+				m = initLinkedList();
+				m = LinkedList.insertAt(i, LinkedList.copy(insert), m);
+				
+				System.out.format("Inserting at index %d: ", i);
+				print(m);
+			} catch(IndexOutOfBoundsException e) {
+				System.out.format("Inserting at index %d: ", i);
+				System.out.println(e);
+			}
+		}
+	}
+	
+	@Before
+	private static LinkedList initLinkedList() {
+		LinkedList m = LinkedList.empty();
+		
+		for(int k = 0; k < 5; k++) {
+			m = LinkedList.append(m, k);
+		}
+		return m;
+	}
+	
+	/**
+	 * Prints a seperator
+	 */
+	private static void printSeperator() {
+		for(int i = 0; i<6; i++) {
+			System.out.print("-");
+		}
+		
+		for(int j = 0; j < 2; j++) {
+			System.out.println();
+		}
+		
 	}
 
 	/**
