56ac2b6451502e69f1fbefecb44e81d55fdcf814
B3tah3 <33493099+B3tah3@users.noreply.github.com>
2021-05-10 10:43:55 +0200

    Added text Reconstruction, known mistake: first letter

--- Algorithmen/textCodierung/TextCoding.java
+++ Algorithmen/textCodierung/TextCoding.java
@@ -8,6 +8,10 @@ public class TextCoding {
 
 	static LinkedList alphabet = generateAlphabet(111);
 
+	/*
+	 * Main Method that processes the text into it's compressed form and then
+	 * reconstructs it
+	 */
 	public static void main(String[] args) throws IOException {
 
 		CharacterCoding characterCoding = new CharacterCoding();
@@ -24,8 +28,22 @@ public class TextCoding {
 
 		characterCoding.writeToFile(path + "faust_compressed.txt", textResult);
 
+		// Reconstructing
+		int[] textReconstruct = new int[textResult.length];
+		for (int i = textResult.length - 1; i > 0; i--) {
+			int index = textResult[i];
+			textReconstruct[i] = sortBack(index);
+		}
+
+		characterCoding.writeToFile(path + "faust_reconstructed.txt", textReconstruct);
+
 	}
 
+	/**
+	 * 
+	 * @param character the charater to be searched and moved to the front
+	 * @return the index where the character was found
+	 */
 	private static int moveToFront(int character) {
 
 		int charIndex = LinkedList.getIndexByValue(character, alphabet);
@@ -35,6 +53,21 @@ public class TextCoding {
 		return charIndex;
 	}
 
+	private static int sortBack(int index) {
+
+		int character = LinkedList.first(alphabet);
+
+		alphabet = LinkedList.moveFromTo(0, index, alphabet);
+
+		return character;
+	}
+
+	/**
+	 * Creates a Linked List filled with numbers from 0 to m-1
+	 * 
+	 * @param m the length of the desired alphabet
+	 * @return the LinkedList
+	 */
 	private static LinkedList generateAlphabet(int m) {
 		LinkedList alphabet = LinkedList.empty();
 		for (int i = 0; i < m; i++) {
